5284840537664cd8bc80a28fa320960d622fa920
Junhwa Shin <junhwa1000@naver.com>
2024-03-14 00:27:01 +0900

    q13

--- Python Algorithm Interview/q13.py
+++ Python Algorithm Interview/q13.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 #펠린드롬 연결 리스트
 #1->2 false, 1->2->2->1 true
 from typing import List
@@ -32,28 +31,3 @@ class Solution:
 sol = Solution()
 ln = ListNode([1,2,2,1])
 print(sol.isPalindrome(ln))
-=======
-#펠린드롬 연결 리스트
-#1->2 false, 1->2->2->1 true
-from typing import List
-from typing import ListNode
-
-def isPalindrome(self, head: ListNode)->bool:
-    q: List = []
-    
-    if not head:
-        return True
-    
-    node = head
-    
-    while node is not None:
-        q.append(node.val)
-        node = node.next
-    
-    while len(q)>1:
-        if q.pop(0) != q.pop():
-            return False
-        
-    return True
->>>>>>> 3ae5b6e5aff0d2d84dd7638349b3981d1e6a7c45
-        
